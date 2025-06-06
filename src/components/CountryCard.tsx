9ab35ce8b95287e6bc97dfc05ed30fbc3f8d5fbf
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-06-05 21:37:16 -0300

    feat: improve responsiveness and define winner and loser logic

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,24 @@
+export default function CountryCard({ index, value }) {
+  return (
+    <div key={index} className="country-card">
+      <img src={value.flags.png} alt={value.name} />
+      <div className="card__texts">
+        <h3 className="card__title">{value.name}</h3>
+        <div className="card__infos">
+          <p>
+            <span className="card__info-title">Population: </span>
+            {value.population}
+          </p>
+          <p>
+            <span className="card__info-title">Region: </span>
+            {value.region}
+          </p>
+          <p>
+            <span className="card__info-title">Capital: </span>
+            {value.capital}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
