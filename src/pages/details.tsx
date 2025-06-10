b50eecd0a9fd4b410ad7bd1df86c0fcf8c768485
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-06-09 21:24:57 -0300

    feat(details): add border countries and interactivity on click

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -5,7 +5,12 @@ import "../styles/details.css";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Details() {
-  const [allCountries, setAllCountries] = useState("");
+  const [allCountries, setAllCountries] = useState([]);
+  const [borderCountries, setBorderCountries] = useState([]);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const country = location.state;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,24 +21,24 @@ export default function Details() {
     fetchData();
   }, []);
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const country = location.state;
+  useEffect(() => {
+    if (!country || allCountries.length === 0) return;
+
+    //Get all details from bordering countries
+    const arrBorderCountries = allCountries.filter((value) =>
+      country.borders.includes(value.alpha3Code)
+    );
+    setBorderCountries(arrBorderCountries);
+  }, [allCountries, country]);
 
   if (!country) {
     return <p>Country data not found.</p>; // fallback for direct access
-  }
-
-  function getBorderCountries() {
-    const borderCountries = country.borders;
   }
 
   function getInfo(key: "currencies" | "languages"): string {
     const info = country[key];
     return info.map((item) => item.name).join(", ");
   }
-
-  getBorderCountries();
 
   return (
     <div className="main-container">
@@ -42,48 +47,68 @@ export default function Details() {
         Back
       </button>
       <div className="details-container">
-        <img src={country.flags.png} alt={country.name} />
+        <img
+          src={country.flags.png}
+          alt={country.name}
+          className="country-flag"
+        />
         <div>
           <h2>{country.name}</h2>
-          <div className="info-text-container">
-            <p className="info__text">
-              <span className="info__title">Native Name: </span>
-              {country.nativeName}
-            </p>
-            <p className="info__text">
-              <span className="info__title">Population: </span>
-              {country.population}
-            </p>
-            <p className="info__text">
-              <span className="info__title">Region: </span>
-              {country.region}
-            </p>
-            <p className="info__text">
-              <span className="info__title">Sub Region: </span>
-              {country.subregion}
-            </p>
-            <p className="info__text">
-              <span className="info__title">Capital: </span>
-              {country.capital}
-            </p>
-          </div>
-          <div className="info-text-container">
-            <p className="info__text">
-              <span className="info__title">Top Level Domain: </span>
-              {country.topLevelDomain}
-            </p>
-            <p className="info__text">
-              <span className="info__title">Currencies: </span>
-              {getInfo("currencies")}
-            </p>
-            <p className="info__text">
-              <span className="info__title">Languages: </span>
-              {getInfo("languages")}
-            </p>
+          <div className="info-text">
+            <div className="info-text-container">
+              <p className="info__text">
+                <span className="info__title">Native Name: </span>
+                {country.nativeName}
+              </p>
+              <p className="info__text">
+                <span className="info__title">Population: </span>
+                {country.population}
+              </p>
+              <p className="info__text">
+                <span className="info__title">Region: </span>
+                {country.region}
+              </p>
+              <p className="info__text">
+                <span className="info__title">Sub Region: </span>
+                {country.subregion}
+              </p>
+              <p className="info__text">
+                <span className="info__title">Capital: </span>
+                {country.capital}
+              </p>
+            </div>
+            <div className="info-text-container">
+              <p className="info__text">
+                <span className="info__title">Top Level Domain: </span>
+                {country.topLevelDomain}
+              </p>
+              <p className="info__text">
+                <span className="info__title">Currencies: </span>
+                {getInfo("currencies")}
+              </p>
+              <p className="info__text">
+                <span className="info__title">Languages: </span>
+                {getInfo("languages")}
+              </p>
+            </div>
           </div>
           <div className="info-text-container">
             <h3 className="info__title-border-countries">Border Countries:</h3>
-            <div className="border-countries-container">{}</div>
+            <div className="border-countries-container">
+              {borderCountries.map((country, index) => {
+                return (
+                  <div
+                    className="border-country-card"
+                    key={index}
+                    onClick={() => {
+                      navigate("/details", { state: country });
+                    }}
+                  >
+                    {country.name}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
