600938a647f9ce825ef9473adb92d0af5cd7bb20
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-06-23 23:10:32 -0300

    refactor: change all tags to <div>

--- src/components/LoadingCard.tsx
+++ src/components/LoadingCard.tsx
@@ -3,10 +3,10 @@ import "../styles/loadingCard.css";
 export default function LoadingCard() {
   return (
     <div className="loading-card">
-      <div className="loading__image"></div>
+      <div className="loading__image" />
       <div className="loading__content">
-        <h2 className="loading__title"></h2>
-        <p className="loading__text"></p>
+        <div className="loading__title" />
+        <div className="loading__text" />
       </div>
     </div>
   );
